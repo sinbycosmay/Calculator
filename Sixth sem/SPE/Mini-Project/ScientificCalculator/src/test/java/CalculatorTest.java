11042fdabf8cc097850229cbdfd70674cc7e0ca8
TanishqJ13 <tanishqj13@gmail.com>
2021-03-12 15:42:38 +0530

    commit4

--- Sixth sem/SPE/Mini-Project/ScientificCalculator/src/test/java/CalculatorTest.java
+++ Sixth sem/SPE/Mini-Project/ScientificCalculator/src/test/java/CalculatorTest.java
@@ -11,12 +11,27 @@ class CalculatorTest {
 
         int actual = c.factorial(5);
         assertEquals(120, actual);
+
+        int actual2 = c.factorial(0);
+        assertEquals(1, actual2);
+
+        int actual3 = c.factorial(1);
+        assertEquals(1,actual3);
+
+        int actual4 = c.factorial(-1);
+        assertEquals(1,actual4);
     }
 
     @Test
     void power() {
         double actual = c.power(2,3);
         assertEquals(8, actual);
+
+        double actual2 = c.power(-1,-1);
+        assertEquals(-1, actual2);
+
+        double actual3 = c.power(2.3,4.5);
+        assertEquals(42.43998894277659, actual3);
     }
 
     @Test
@@ -30,16 +45,21 @@ class CalculatorTest {
         double actual2 = c.sq_root(0);
         assertEquals(0, actual2);
 
-        //double actual3 = c.sq_root(-1);
-        //assertEquals("NaN", actual3);
+        double actual3 = c.sq_root(-1);
+        assertEquals(Double.NaN, actual3);
 
     }
 
     @Test
     void log() {
         double actual = c.log(1);
-
         assertEquals(0, actual);
+
+        double actual2 = c.log(0);
+        assertEquals(Double.NEGATIVE_INFINITY, actual2);
+
+        double actual3 = c.log(-1);
+        assertNotEquals(Double.NEGATIVE_INFINITY, actual3);
     }
 
 
