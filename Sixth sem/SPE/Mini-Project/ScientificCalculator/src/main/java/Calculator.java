380b3ceb6de09a7df025a7367ea733bb49a6d0c2
TanishqJ13 <tanishqj13@gmail.com>
2021-03-12 10:37:40 +0530

    Initial commit

---
+++ Sixth sem/SPE/Mini-Project/ScientificCalculator/src/main/java/Calculator.java
@@ -0,0 +1,90 @@
+import java.util.*;
+import java.lang.Math;
+
+public class Calculator {
+
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("User menu option: \n 1.Square root function \n 2. Factorial function \n 3. Natural Logarithm(base e) \n 4. Power function \n");
+        int option = sc.nextInt();
+
+        Calculator obj = new Calculator();
+
+        if(option==1)
+        {
+            //Takes only numbers >= 0
+            //Error handling done, returns NaN if negative number
+            System.out.print("Enter the number for which you wish to calculate: \n");
+            double num = sc.nextDouble();
+
+            System.out.println("Square root of " + num + " is " + obj.sq_root(num));
+        }
+        else if(option==2)
+        {
+            //Takes only integer as input
+            System.out.print("Enter the number for which you wish to calculate: \n");
+            int num = sc.nextInt();
+
+            if (num < 0)
+            {
+                System.out.print("Factorial of a negative number is not defined. \n");
+            }
+            else
+            {
+                System.out.println("Factorial of " + num + " is " + obj.factorial(num));
+            }
+        }
+        else if(option==3)
+        {
+            //Negative no. -> NaN, 0 -> -Infinity, Infinity -> Infinity
+            System.out.print("Enter the number for which you wish to calculate: \n");
+            double num = sc.nextDouble();
+
+            System.out.println("Natural Logarithm(base e) of " + num + " is " + obj.log(num));
+        }
+        else if(option==4)
+        {
+            System.out.print("Enter the number for which you wish to calculate: \n");
+            double num = sc.nextDouble();
+
+            System.out.print("Enter the exponent number: \n");
+            double exp = sc.nextDouble();
+            System.out.println("Power of " + num + " is " + obj.power(num, exp));
+        }
+        else
+        {
+            //wrong input
+            System.out.print("Wrong input for selecting the options. Kindly choose from 1,2,3,4 \n");
+        }
+    }
+
+    public int factorial(int n)
+    {
+        int res = 1;
+        for(int i=2; i<=n; i++)
+        {
+            res = res*i;
+        }
+        return res;
+    }
+
+    public double sq_root(double n)
+    {
+        double ans = Math.sqrt(n);
+        return ans;
+    }
+
+    public double log(double n)
+    {
+        double ans = Math.log(n);
+        return ans;
+    }
+
+    public double power(double a, double b)
+    {
+        double ans = Math.pow(a,b);
+        return ans;
+    }
+}
---
+++ Sixth sem/SPE/Mini-Project/ScientificCalculator/src/test/java/CalculatorTest.java
@@ -0,0 +1,50 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CalculatorTest {
+
+    Calculator c = new Calculator();
+
+    @Test
+    void factorial() {
+
+        int actual = c.factorial(5);
+        assertEquals(120, actual);
+    }
+
+    @Test
+    void power() {
+        double actual = c.power(2,3);
+        assertEquals(8, actual);
+    }
+
+    @Test
+    void sq_root() {
+        double actual = c.sq_root(2.4);
+        assertEquals(1.5491933384829668, actual);
+
+        double actual1 = c.sq_root(4);
+        assertEquals(2, actual1);
+
+        double actual2 = c.sq_root(0);
+        assertEquals(0, actual2);
+
+        //double actual3 = c.sq_root(-1);
+        //assertEquals("NaN", actual3);
+
+    }
+
+    @Test
+    void log() {
+        double actual = c.log(1);
+
+        assertEquals(0, actual);
+    }
+
+
+
+
+
+
+}
